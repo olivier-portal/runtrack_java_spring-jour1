f59c7270a2bafc089c132962488a7fa250fe63fe
olivier-portal <olivierportal09@gmail.com>
2025-09-02 16:06:39 +0200

    job4 ok

--- demo/demo/src/main/java/com/example/demo/controller/HelloController.java
+++ demo/demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -3,13 +3,17 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.beans.factory.annotation.Value;
 
 @Controller
 public class HelloController {
 
+    @Value("${greetings.message}")
+    private String greetingMessage;
+
     @GetMapping("/")
     @ResponseBody
     public String hello() {
-        return "Hello World";
+        return greetingMessage;
     }
 }
